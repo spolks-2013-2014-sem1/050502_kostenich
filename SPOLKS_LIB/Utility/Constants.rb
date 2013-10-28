44df7ca1fc74a2fb10caffed76499ebd774fc2c9
Anton Kostenich <anton.kostenich@hotmail.com>
2013-10-29 02:39:23 +0300

    SPOLKS_LIB - Library code refactor

--- SPOLKS_LIB/Utility/Constants.rb
+++ SPOLKS_LIB/Utility/Constants.rb
@@ -1,16 +1,17 @@
 class Constants
-	BACKLOG_VALUE = 5
-	CHUNK_SIZE = 1024 * 1024 * 10
+  BACKLOG_VALUE = 5
+  CHUNK_SIZE = 1024 * 1024 * 10
+  TIMEOUT = 10
 
-	CONNECTED_CLIENT_WAS = "Connected client is:"
-	SERVER_IS_OFFLINE = "Server is currently offline."
-	CLIENT_WAS_DISCONNECTED = "Client was disconnected."
-	IMPLEMENT_THIS_METHOD = "Implement this method in subclass"
-	STOP_MESSAGE = "stop"
+  CONNECTED_CLIENT_WAS = "Connected client is:"
+  SERVER_IS_OFFLINE = "Server is currently offline."
+  CLIENT_WAS_DISCONNECTED = "Client was disconnected."
+  IMPLEMENT_THIS_METHOD = "Implement this method in subclass"
+  STOP_MESSAGE = "stop"
 
-	DEFAULT_HOST_NAME = "localhost"
-	DEFAULT_PORT_NUMBER = 2200
+  DEFAULT_HOST_NAME = "localhost"
+  DEFAULT_PORT_NUMBER = 2200
 
-	READ_FILE_FLAG = 'rb'
-	WRITE_FILE_FLAG = 'w'
+  READ_FILE_FLAG = 'rb'
+  WRITE_FILE_FLAG = 'w'
 end
--- SPOLKS_LIB/Utility/FileTransfer.rb
+++ SPOLKS_LIB/Utility/FileTransfer.rb
@@ -1,17 +1,20 @@
 require '../SPOLKS_LIB/Utility/Constants.rb'
 
 class FileTransfer
-	def initialize(file_path, mode)
-		@file = File.open(file_path, mode)
+  def initialize(file_path, mode)
+    @file = File.open(file_path, mode)
+  end
+  def divide_file_by_chunks
+	while(chunk = @file.read(Constants::CHUNK_SIZE))
+	  if block_given?
+	    yield chunk
+	  end
 	end
-	def divide_file_by_chunks
-		while(chunk = @file.read (Constants::CHUNK_SIZE))
-			if block_given?
-				yield chunk
-			end
-		end
-	end
-	def create_file_with_chunks(chunk)
-		@file.write(chunk)
-	end
+  end
+  def create_file_with_chunks(chunk)
+    @file.write(chunk)
+  end
+  def close
+    @file.close  
+  end
 end
