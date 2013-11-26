91c177ebeeaff4e78fe740b2feff6729080e8771
a.kostenich <anton.kostenich@hotmail.com>
2013-11-26 02:43:40 +0000

    Lab2 - Migrate to default File class

--- Lab2/client.rb
+++ Lab2/client.rb
@@ -4,26 +4,26 @@ require '../SPOLKS_LIB/Utility/FileTransfer.rb'
 class Client
   def initialize(socket, filepath)
     @socket = socket
-	@file_transfer = FileTransfer.new(filepath, Constants::WRITE_FILE_FLAG)
+    @file = File.open(filepath, Constants::WRITE_FILE_FLAG)
   end
   def connect(port_number, host_name)
     sockaddr = Socket.sockaddr_in(port_number, host_name)
-	@socket.connect(sockaddr)
-	self.receive_file {|chunk| @file_transfer.create_file_with_chunks(chunk)}
+    @socket.connect(sockaddr)
+    self.receive_file {|chunk| @file.write(chunk)}
+    @file.close
   end
   def receive_file
-  	loop do
-  	  rs, _ = IO.select([@socket.socket], nil, nil, Constants::TIMEOUT)
-	  break unless rs
+    loop do
+      rs, _ = IO.select([@socket.socket], nil, nil, Constants::TIMEOUT)
 
-	  rs.each do |s|
+      rs.each do |s|
         data = s.recv(Constants::CHUNK_SIZE)
-	    return if data.empty?
-	    if block_given?
-	  	  yield data
-	    end
-	  end
+        return if data.empty?
+        if block_given?
+          yield data
+        end
+      end
 	
-  	end
+    end
   end
 end
--- Lab2/server.rb
+++ Lab2/server.rb
@@ -4,16 +4,19 @@ require '../SPOLKS_LIB/Utility/FileTransfer.rb'
 class Server
   def initialize(socket, filepath)
     @socket = socket
-	  @file_transfer = FileTransfer.new(filepath, Constants::READ_FILE_FLAG)
+	  @file = File.open(filepath, Constants::READ_FILE_FLAG)
   end
   def start
 	  @socket.listen
 	  self.send_file
   end
   def send_file
-    @file_transfer.divide_file_by_chunks { |chunk| @socket.client_socket.send(chunk, 0) }
+    while (chunk = @file.read(Constants::CHUNK_SIZE))
+      @socket.client_socket.send(chunk, 0)
+    end
   end
   def stop
 	  @socket.close
+    @file.close
   end
 end
