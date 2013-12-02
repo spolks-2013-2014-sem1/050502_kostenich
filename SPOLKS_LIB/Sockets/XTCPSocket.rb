8288619d9a006bfa650b8d0752c2b8ca38cd5fdb
a.kostenich <anton.kostenich@hotmail.com>
2013-12-02 02:09:32 +0000

    SPOLKS_LIB - Add TCP socket options

--- SPOLKS_LIB/Sockets/XTCPSocket.rb
+++ SPOLKS_LIB/Sockets/XTCPSocket.rb
@@ -3,23 +3,24 @@ require '../SPOLKS_LIB/Sockets/XBasicSocket.rb'
 class XTCPSocket < XBasicSocket
   attr_accessor :client_socket, :socket
   def initialize(port_number, host_name)
-	@socket = Socket.new(Socket::AF_INET6, Socket::SOCK_STREAM, 0)
-	@sockaddr = Socket.sockaddr_in(port_number, host_name)
+    @socket = Socket.new(Socket::AF_INET6, Socket::SOCK_STREAM, 0)
+    @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+    @sockaddr = Socket.sockaddr_in(port_number, host_name)
   end
   def listen
-	@socket.bind(@sockaddr)
-	@socket.listen(Constants::BACKLOG_VALUE)
-	self.accept
+    @socket.bind(@sockaddr)
+    @socket.listen(Constants::BACKLOG_VALUE)
+    self.accept
   end
   def connect(sockaddr)
-	@socket.connect(sockaddr)
+    @socket.connect(sockaddr)
   end
   def accept
     @client_socket, client_addrinfo = @socket.accept
-	inspect_client(client_addrinfo)
+    inspect_client(client_addrinfo)
   end
   def close
     @client_socket.close
-	@socket.close
+    @socket.close
   end
 end
