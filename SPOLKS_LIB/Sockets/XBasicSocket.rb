b91cfaf5a9b99ed5103e54df0aa093d66e4ebf02
a.kostenich <anton.kostenich@hotmail.com>
2013-12-02 04:27:53 +0000

    SPOLKS_LIB - Code cleanup

--- SPOLKS_LIB/Sockets/XBasicSocket.rb
+++ SPOLKS_LIB/Sockets/XBasicSocket.rb
@@ -5,19 +5,15 @@ class XBasicSocket
 	def listen
 		raise NotImplementedError, Constants::IMPLEMENT_THIS_METHOD
 	end
-
 	def connect
 		raise NotImplementedError, Constants::IMPLEMENT_THIS_METHOD
 	end
-
 	def close
 		raise NotImplementedError, Constants::IMPLEMENT_THIS_METHOD
 	end
-
 	def accept
 		raise NotImplementedError, Constants::IMPLEMENT_THIS_METHOD
 	end
-
 	def inspect_client (addrinfo)
 		puts(Constants::CONNECTED_CLIENT_WAS)
 		Socket.getnameinfo(addrinfo).each { |line| puts(line) }
--- SPOLKS_LIB/Utility/Constants.rb
+++ SPOLKS_LIB/Utility/Constants.rb
@@ -1,12 +1,13 @@
 class Constants
   BACKLOG_VALUE = 5
-  CHUNK_SIZE = 1024 * 64
+  CHUNK_SIZE = 1024 * 8
   TIMEOUT = 10
 
   CONNECTED_CLIENT_WAS = "Connected client is:"
   SERVER_IS_OFFLINE = "Server is currently offline."
   CLIENT_WAS_DISCONNECTED = "Client was disconnected."
   IMPLEMENT_THIS_METHOD = "Implement this method in subclass"
+  UDP_SOCKET_DOES_NOT_SUPPORT_THIS_OPERATION = "UDP socket does not support this operation"
   STOP_MESSAGE = "stop"
 
   DEFAULT_HOST_NAME = "localhost"
@@ -16,4 +17,5 @@ class Constants
   WRITE_FILE_FLAG = 'w'
 
   OOB_MESSAGE = '#'
+  UDP_MESSAGE = '!'
 end
