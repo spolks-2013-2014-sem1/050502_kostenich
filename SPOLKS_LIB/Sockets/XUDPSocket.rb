0e2b046c3e40c357c9020a0429f42613bd7deff0
a.kostenich <anton.kostenich@hotmail.com>
2013-12-02 04:27:20 +0000

    SPOLKS_LIB - Implement UDP socket

---
+++ SPOLKS_LIB/Sockets/XUDPSocket.rb
@@ -0,0 +1,31 @@
+require_relative "XBasicSocket.rb"
+
+class XUDPSocket < XBasicSocket
+  attr_accessor :socket, :client_sockaddr
+  def initialize(port_number, host_name)
+    @socket = Socket.new(Socket::AF_INET6, Socket::SOCK_DGRAM, 0)
+    @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+    @sockaddr = Socket.sockaddr_in(port_number, host_name)
+  end
+  def listen
+    @socket.bind(@sockaddr)
+    self.accept
+  end
+  def accept
+    while(1)
+      msg, @client_sockaddr = @socket.recvfrom(1)
+      if (msg == Constants::UDP_MESSAGE)
+        break
+      end
+    end
+  end
+  def send(msg, flags, dst_addr)
+    @socket.send(msg, flags, dst_addr)
+  end
+  def connect(sockaddr)
+    @socket.connect(sockaddr)
+  end
+  def close
+    @socket.close
+  end
+end
