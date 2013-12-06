ac5de0a022e7a5fc2708fd374e3aab376562e3b1
a.kostenich <anton.kostenich@hotmail.com>
2013-12-06 03:28:18 +0000

    Lab5 - Initial commit

---
+++ Lab5/server.rb
@@ -0,0 +1,22 @@
+require '../SPOLKS_LIB/Sockets/XTCPSocket.rb'
+
+class UDPServer
+  def initialize(socket, filepath)
+    @socket = socket
+    @file = File.open(filepath, Constants::READ_FILE_FLAG)
+  end
+  def start
+	  @socket.listen
+	  self.send_file
+  end
+  def send_file
+    while (chunk = @file.read(Constants::CHUNK_SIZE))
+      @socket.send(chunk, 0, @socket.client_sockaddr)
+    end
+    @socket.send(Constants::UDP_MESSAGE, 0, @socket.client_sockaddr)
+  end
+  def stop
+	  @socket.close
+    @file.close
+  end
+end
